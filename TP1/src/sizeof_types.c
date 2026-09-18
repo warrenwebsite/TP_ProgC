7ac2aafdc8a2c5c72879a0e58c886c7cd7042b14
Warren <warren@2a01cb014012d32619b54c5fdaf3e438.ipv6.abo.wanadoo.fr>
2026-09-18 11:29:14 +0200

    Exercice 1.3 valide avec executable

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,1 +1,30 @@
 
+#include <stdio.h>
+
+int main() {
+    printf("char                    : %zu octet(s)\n", sizeof(char));
+    printf("signed char             : %zu octet(s)\n", sizeof(signed char));
+    printf("unsigned char           : %zu octet(s)\n", sizeof(unsigned char));
+
+    printf("short                   : %zu octet(s)\n", sizeof(short));
+    printf("signed short            : %zu octet(s)\n", sizeof(signed short));
+    printf("unsigned short          : %zu octet(s)\n", sizeof(unsigned short));
+
+    printf("int                     : %zu octet(s)\n", sizeof(int));
+    printf("signed int              : %zu octet(s)\n", sizeof(signed int));
+    printf("unsigned int            : %zu octet(s)\n", sizeof(unsigned int));
+
+    printf("long int                : %zu octet(s)\n", sizeof(long int));
+    printf("signed long int         : %zu octet(s)\n", sizeof(signed long int));
+    printf("unsigned long int       : %zu octet(s)\n", sizeof(unsigned long int));
+
+    printf("long long int           : %zu octet(s)\n", sizeof(long long int));
+    printf("signed long long int    : %zu octet(s)\n", sizeof(signed long long int));
+    printf("unsigned long long int  : %zu octet(s)\n", sizeof(unsigned long long int));
+
+    printf("float                   : %zu octet(s)\n", sizeof(float));
+    printf("double                  : %zu octet(s)\n", sizeof(double));
+    printf("long double             : %zu octet(s)\n", sizeof(long double));
+
+    return 0;
+}
