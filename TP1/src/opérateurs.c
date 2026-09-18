6a7f92f313d1f2892ab4e2649d01490f0251e5b0
Warren <warren@2a01cb014012d32619b54c5fdaf3e438.ipv6.abo.wanadoo.fr>
2026-09-18 11:35:11 +0200

    Exercice 1.5 termine avec executable

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
 
+int main() {
+    int a = 16;
+    int b = 3;
 
+    printf("Addition : %d\n", a + b);
+    printf("Soustraction : %d\n", a - b);
+    printf("Multiplication : %d\n", a * b);
+    printf("Division : %d\n", a / b);
+    printf("Modulo : %d\n", a % b);
+
+    printf("a est égal à b : %s\n", (a == b) ? "true" : "false");
+    printf("a est supérieur à b : %s\n", (a > b) ? "true" : "false");
+
+    return 0;
+}
