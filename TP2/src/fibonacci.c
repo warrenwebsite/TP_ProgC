dd7b6639e2b909c0583be0c0577dc9151cc5a8df
Warren <warren@2a01cb014012d32619b54c5fdaf3e438.ipv6.abo.wanadoo.fr>
2026-09-18 11:56:24 +0200

    TP2 - Exercice 2.3 termine avec executable

---
+++ TP2/src/fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void) {
+    int n = 7;
+
+    if (n < 0) {
+        return 0;
+    }
+
+    int u0 = 0;
+    int u1 = 1;
+
+    printf("Suite de Fibonacci jusqu'à U%d : ", n);
+
+    if (n == 0) {
+        printf("%d\n", u0);
+        return 0;
+    }
+
+    printf("%d, %d", u0, u1);
+
+    for (int i = 2; i <= n; i++) {
+        int un = u0 + u1;
+        printf(", %d", un);
+        u0 = u1;
+        u1 = un;
+    }
+
+    printf("\n");
+    return 0;
+}
