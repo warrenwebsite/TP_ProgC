9b63391d303b7ffa66417e16a726f5db9fba4358
Warren <warren@2a01cb014012d32619b54c5fdaf3e438.ipv6.abo.wanadoo.fr>
2026-09-18 11:50:35 +0200

    Exercice 1.10 termine avec executable

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main() {
+    int n = 5;
+    int i, j;
+
+    for (i = 1; i <= n; i++) {
+
+        // Afficher les espaces
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        // Afficher les nombres croissants
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // Afficher les nombres décroissants
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    printf("La génération de la pyramide est terminée.\n");
+
+    return 0;
+}
