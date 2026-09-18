5ceba6e17a22465f7cbba5d8b34677087cf0af2e
Warren <warren@2a01cb014012d32619b54c5fdaf3e438.ipv6.abo.wanadoo.fr>
2026-09-18 11:41:14 +0200

    Exercice 1.8 termine avec executable

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,55 @@
+#include <stdio.h>
 
+int main() {
+    int num1 = 16;
+    int num2 = 3;
+    char op = '+';
+
+    switch (op) {
+        case '+':
+            printf("Résultat : %d\n", num1 + num2);
+            break;
+
+        case '-':
+            printf("Résultat : %d\n", num1 - num2);
+            break;
+
+        case '*':
+            printf("Résultat : %d\n", num1 * num2);
+            break;
+
+        case '/':
+            if (num2 != 0) {
+                printf("Résultat : %d\n", num1 / num2);
+            } else {
+                printf("Erreur : division par zéro\n");
+            }
+            break;
+
+        case '%':
+            if (num2 != 0) {
+                printf("Résultat : %d\n", num1 % num2);
+            } else {
+                printf("Erreur : modulo par zéro\n");
+            }
+            break;
+
+        case '&':
+            printf("Résultat : %d\n", num1 & num2);
+            break;
+
+        case '|':
+            printf("Résultat : %d\n", num1 | num2);
+            break;
+
+        case '~':
+            printf("Résultat : %d\n", ~num1);
+            break;
+
+        default:
+            printf("Opérateur invalide\n");
+            break;
+    }
+
+    return 0;
+}
